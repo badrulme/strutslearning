5c1138b005f8ae738b0daaa89a06bd045d522ca4
badrul <badrul.alam@bjitgroup.com>
2022-06-10 15:35:39 +0600

    Using Tags

--- src/main/java/com/badrul/strutslearning/HelloWorldAction.java
+++ src/main/java/com/badrul/strutslearning/HelloWorldAction.java
@@ -5,9 +5,17 @@ import com.opensymphony.xwork2.ActionSupport;
 public class HelloWorldAction extends ActionSupport {
     private MessageStore messageStore;
 
+    private static int helloCount = 0;
+
+    public int getHelloCount() {
+        return helloCount;
+    }
+
+
     @Override
     public String execute() {
         messageStore = new MessageStore() ;
+        helloCount++;
 
         return SUCCESS;
     }
